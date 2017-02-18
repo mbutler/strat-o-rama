6629dcb8b222368f3fd9eddb3a41dd72ae951154
Matthew Butler <matthewtbutler@gmail.com>
2017-02-17 22:11:15 -0600

    working stat assignment
    accurately assigns stats

--- partition.js
+++ partition.js
@@ -22,24 +22,24 @@ function getSubchancesList() {
 
 var card = 
 [
-    {name: '1-2', subchances: 20, result: [], index: 0},
-    {name: '1-3', subchances: 1, result: [], index: 1},
-    {name: '1-4', subchances: 0, result: [], index: 2},
-    {name: '1-5', subchances: 0, result: [], index: 3},
-    {name: '1-6', subchances: 0, result: [], index: 4},
-    {name: '1-7', subchances: 0, result: [], index: 5},
-    {name: '1-8', subchances: 10, result: [], index: 6},
-    {name: '1-9', subchances: 0, result: [], index: 7},
-    {name: '1-10', subchances: 0, result: [], index: 8},
+    {name: '1-2', subchances: 0, result: [], index: 0},
+    {name: '1-3', subchances: 40, result: [], index: 1},
+    {name: '1-4', subchances: 60, result: [], index: 2},
+    {name: '1-5', subchances: 80, result: [], index: 3},
+    {name: '1-6', subchances: 100, result: [], index: 4},
+    {name: '1-7', subchances: 120, result: [], index: 5},
+    {name: '1-8', subchances: 100, result: [], index: 6},
+    {name: '1-9', subchances: 80, result: [], index: 7},
+    {name: '1-10', subchances: 60, result: [], index: 8},
     {name: '1-11', subchances: 40, result: [], index: 9},
     {name: '1-12', subchances: 20, result: [], index: 10},
     {name: '2-2', subchances: 20, result: [], index: 11},
     {name: '2-3', subchances: 40, result: [], index: 12},
     {name: '2-4', subchances: 60, result: [], index: 13},
     {name: '2-6', subchances: 80, result: [], index: 14},
-    {name: '2-6', subchances: 11, result: [], index: 15},
+    {name: '2-6', subchances: 100, result: [], index: 15},
     {name: '2-7', subchances: 120, result: [], index: 16},
-    {name: '2-8', subchances: 0, result: [], index: 17},
+    {name: '2-8', subchances: 100, result: [], index: 17},
     {name: '2-9', subchances: 80, result: [], index: 18},
     {name: '2-10', subchances: 60, result: [], index: 19},
     {name: '2-11', subchances: 40, result: [], index: 20},
@@ -48,20 +48,45 @@ var card =
     {name: '3-3', subchances: 40, result: [], index: 23},
     {name: '3-4', subchances: 60, result: [], index: 24},
     {name: '3-5', subchances: 80, result: [], index: 25},
-    {name: '3-6', subchances: 0, result: [], index: 26},
+    {name: '3-6', subchances: 100, result: [], index: 26},
     {name: '3-7', subchances: 120, result: [], index: 27},
-    {name: '3-8', subchances: 10, result: [], index: 28},
+    {name: '3-8', subchances: 100, result: [], index: 28},
     {name: '3-9', subchances: 80, result: [], index: 29},
     {name: '3-10', subchances: 60, result: [], index: 30},
     {name: '3-11', subchances: 40, result: [], index: 31},
     {name: '3-12', subchances: 20, result: [], index: 32}   
 ]
+/* walk: 280
+
+ hit by pitch: 60
+
+ hit: 620
+
+ double: 110
+
+ triple: 0
+
+ home run: 25
+
+ strikeout: 300
+
+ ground ball A: 340
+
+ ground ball B: 280
+
+ steal rating: E
+
+ flyout A: 0
+
+ flyout B: 220*/
 
 
 
 
 
-let homers = 10
+let homers = 199
+let walks = 280
+let hits = 620
 
 //returns the object that is the best choice for what to subtract subchances
 //handle all subtractions and result setting outside this function in a while loop
@@ -91,4 +116,46 @@ function findBestEntry(current_stat) {
     return matchingEntry
 }
 
-console.log(findBestEntry(homers))
+
+function assignStat(total, text, split = false) {
+    while (total > 0) {
+        var entry = findBestEntry(total)
+        let total_subchances = entry.subchances
+
+        if (entry.subchances <= total) {
+            //it's less than total so just take the whole thing
+            total -= entry.subchances
+            entry.result.push(text)
+            entry.subchances = 0
+        } else {
+            //it's more than the total so we have to split
+            //get the roll by pulling it from the name
+            let roll_label = _.split( entry.name, '-' )
+            //turn the label into the integer and subtract 1 to determine the subchance value of each die increment
+            let roll_value = ( _.toInteger( roll_label[1] ) - 1 )
+            //subtract the amount from the entry          
+            entry.subchances -= total
+            //find upper value of the d20 roll
+            let upper_range = ( ( total_subchances - entry.subchances) / total_subchances ) * 20
+
+            //add the label
+            if ( upper_range == 1 ) {
+                entry.result.push( text + " 1" )
+            } else {
+                entry.result.push( text + " 1-" + upper_range )
+            } 
+            
+            total = 0      
+        }
+    }
+
+    return total 
+}
+
+homers = assignStat(homers, "homerun")
+walks = assignStat(walks, "walk")
+hits = assignStat(hits, "single")
+
+console.log(card)
+
+
